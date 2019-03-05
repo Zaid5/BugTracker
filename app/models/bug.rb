a28548d885a7e112342d816e761cd6571e5395b7
Zaid Iqbal <zaid.teknuk@gmail.com>
2019-03-05 21:10:03 +0500

    added some design and updated form and updated status button actions

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -3,7 +3,7 @@ class Bug < ApplicationRecord
 	has_many :comments, dependent: :destroy
 	belongs_to :user
 
-	has_attached_file :image, styles: { main: "300x300#", thumb: "100x100>#" }
+	has_attached_file :image, styles: { main: "200x200#", thumb: "100x100>#" }
 	do_not_validate_attachment_file_type :image
 	has_attached_file :document
 	do_not_validate_attachment_file_type :document
