af8e74465a3e73faab1304c20b94f10cfa77397c
Zaid Iqbal <zaid.teknuk@gmail.com>
2019-03-05 15:25:45 +0500

    removed conflicting migrations of user_id and comments

--- db/migrate/20190305095113_add_user_id_to_bugs.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToBugs < ActiveRecord::Migration[5.2]
-  def change
-    add_column :bugs, :user_id, :integer
-  end
-end
--- db/migrate/20190305095202_add_comments_to_bugs.rb
+++
@@ -1,5 +0,0 @@
-class AddCommentsToBugs < ActiveRecord::Migration[5.2]
-  def change
-    add_column :bugs, :comment, :string
-  end
-end
