b4fdb342604ec791c2d40684d1eceb5fc5c8095b
Zaid Iqbal <zaid.teknuk@gmail.com>
2019-03-07 20:23:09 +0500

    updated domain name in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,11 +70,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
-  
+
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                  587,
-  domain:               'https://trackbug.herokuapp.com',
+  domain:               'heroku.com',
   user_name:            'zaid.teknuk@gmail.com',
   password:             "scorpion.zaid",
   authentication:       'plain',
